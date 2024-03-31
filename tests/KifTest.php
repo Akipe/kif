c47812c51b894b408a48bd07fc31c1448111784f
Julien Milletre Akipe <code.julien@milletre.fr>
2024-03-31 17:30:16 +0200

    Add unit test for checking HTML output result

---
+++ tests/KifTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+use Akipe\Kif\Kif;
+use PHPUnit\Framework\TestCase;
+
+final class KifTest extends TestCase
+{
+    public function testHtmlGeneratedCorresponding(): void
+    {
+        $input = file_get_contents(__DIR__ . "/KifTest/input.qif");
+        $output = file_get_contents(__DIR__ . "/KifTest/output.html");
+
+        $outputGenerated = (new Kif($input))->getHtml();
+
+        $this->assertEquals(
+            $output,
+            $outputGenerated
+        );
+    }
+}
